fbc44397a3541feda79a2f940e9d337d559215d7
椋本修平 <mukuhonshuuhei@mukuhonshuuheinoMacBook-Air.local>
2022-07-18 18:13:58 +0900

    fix:
    routesの三箇所に半角のスペースが無駄に存在していた。
    そのため、削除したものをcommitした。

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
   get 'users/index'
   get 'users/account'
   get 'users/profile'
-  get 'rooms/posts' 
+  get 'rooms/posts'
   resources:rooms , only: [:new, :create, :update ,:show] do
     collection do
       get 'search'
     end
   end
-  resources:users , only: [:index , :update] 
-  resources:reservations , only: [:index ,:new, :create] 
+  resources:users , only: [:index , :update]
+  resources:reservations , only: [:index ,:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
