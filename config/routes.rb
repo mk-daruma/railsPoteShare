5021357990fae9be5e41728ace52e00eb1625d3f
mk-daruma <mk69sh@gmail.com>
2022-10-13 14:26:32 +0900

    popoを追加

--- config/routes.rb
+++ config/routes.rb
@@ -18,3 +18,5 @@ Rails.application.routes.draw do
   resources:reservations , only: [:index ,:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+popo
